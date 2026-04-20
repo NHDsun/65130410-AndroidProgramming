0cbe009830e4eb3a0d9632fdc99cd9d273b9b8ef
NHDsun <huydatne@gmail.com>
2026-04-20 08:23:02 +0700

    nộp bài lần 4

--- thigk2nguyenhuydat/src/main/java/com/example/thigk2nguyenhuydat/Bai3Activity.java
+++ thigk2nguyenhuydat/src/main/java/com/example/thigk2nguyenhuydat/Bai3Activity.java
@@ -1,4 +1,34 @@
 package com.example.thigk2nguyenhuydat;
 
-public class Bai3Activity {
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Bai3Activity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<DiaDiem> arrayList;
+    DiaDiemAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai3);
+
+        listView = findViewById(R.id.listView);
+
+        arrayList = new ArrayList<>();
+
+        arrayList.add(new DiaDiem(R.drawable.vinwonders, "VinWonders Nha Trang", "Đảo Hòn Tre"));
+        arrayList.add(new DiaDiem(R.drawable.honchong, "Hòn Chồng", "Vĩnh Phước"));
+        arrayList.add(new DiaDiem(R.drawable.thapba, "Tháp Bà Ponagar", "2 Tháng 4"));
+        arrayList.add(new DiaDiem(R.drawable.vienhai, "Viện Hải Dương Học", "Cầu Đá"));
+        arrayList.add(new DiaDiem(R.drawable.chualongson, "Chùa Long Sơn", "23/10"));
+
+        adapter = new DiaDiemAdapter(this, arrayList);
+        listView.setAdapter(adapter);
+    }
 }
---
+++ thigk2nguyenhuydat/src/main/java/com/example/thigk2nguyenhuydat/Bai3DiaDiem.java
@@ -0,0 +1,16 @@
+package com.example.thigk2nguyenhuydat;
+
+public class Bai3DiaDiem {
+    int hinh;
+    String ten, diaChi;
+
+    public Bai3DiaDiem(int hinh, String ten, String diaChi) {
+        this.hinh = hinh;
+        this.ten = ten;
+        this.diaChi = diaChi;
+    }
+
+    public int getHinh() { return hinh; }
+    public String getTen() { return ten; }
+    public String getDiaChi() { return diaChi; }
+}
