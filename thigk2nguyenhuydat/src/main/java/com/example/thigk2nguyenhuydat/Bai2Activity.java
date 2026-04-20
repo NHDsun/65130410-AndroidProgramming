2dcf378157d5db5caba8f6e1e646223d5158388e
NHDsun <huydatne@gmail.com>
2026-04-20 08:07:03 +0700

    Nộp bài lần 3

--- thigk2nguyenhuydat/src/main/java/com/example/thigk2nguyenhuydat/Bai2Activity.java
+++ thigk2nguyenhuydat/src/main/java/com/example/thigk2nguyenhuydat/Bai2Activity.java
@@ -1,4 +1,40 @@
 package com.example.thigk2nguyenhuydat;
 
-public class Bai2Activity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bai2Activity extends AppCompatActivity {
+
+    ListView listView;
+    String[] data = {
+            "Hà Nội",
+            "Hải Phòng",
+            "Đà Nẵng",
+            "Khánh Hòa",
+            "Cần Thơ",
+            "Huế",
+            "TP Hồ Chí Minh",
+            "Bình Dương",
+            "Đắk Lắk",
+            "Nguyễn Huy Đạt"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2);
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+
+        listView.setAdapter(adapter);
+    }
 }
